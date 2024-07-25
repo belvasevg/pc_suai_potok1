ac5508e050fc49d666d51cee6056255d683a83ab
Vasiliy <62217397+belvasevg@users.noreply.github.com>
2024-07-25 20:48:49 +0300

    Добавлены импортирования файлов в main

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 if __name__ =="__main__":
+    from minus import *
+    from plus import *
     n1 = int(input("Первое число: "))
     n2 = int(input("Второе число: "))
     oper = input("Оператор: ")
